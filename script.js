4b8b85172a5c120656ff9be14de477ac836e40d7
arjunkr2001 <arjunradhakrishnan2001@gmail.com>
2023-05-19 12:20:04 +0530

    audio track for both, cleaned js

--- script.js
+++ script.js
@@ -50,26 +50,7 @@ localVdoElmnt.addEventListener('loadedmetadata',e=>{
   let dctx = dc.getContext('2d')
   let x,y,px,py
 
-  //dctx.strokeStyle = drawColor
-
-  // let audioStream
-  // let contraints = {
-  //   video: false,
-  //   audio: true
-  // }
 async function startLocalVideo(){
-    // return navigator.mediaDevices.getUserMedia({video:true,audio:true})
-    //     .then(stream =>{
-    //         localStream = stream
-    //         localVdoElmnt.srcObject = stream
-    //         localVdoElmnt.onloadedmetadata = ()=> localVdoElmnt.play()
-    //     })
-    // navigator.mediaDevices.getUserMedia(contraints)
-    //   .then(audStream =>{
-    //     audioStream = audStream
-    //   //localVdoElmnt.srcObject = stream
-    //   //localVdoElmnt.onloadedmetadata = ()=> localVdoElmnt.play()
-    //   })
     const camera = new Camera(localVdoElmnt, {
         onFrame: async () => {
           await hands.send({image: localVdoElmnt});
@@ -78,7 +59,6 @@ async function startLocalVideo(){
       return camera.start();
 }
 const hands = new Hands({locateFile: (file) => {
-    console.log(file)
     return `hands/${file}`;
   }});
   hands.setOptions({
@@ -96,12 +76,10 @@ const hands = new Hands({locateFile: (file) => {
     ctx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
     if (results.multiHandLandmarks) {
         for (const landmarks of results.multiHandLandmarks) {
-          //console.log(landmarks[12].z)
           px = x
           py = y
           x = landmarks[8].x*vw
           y = landmarks[8].y*vh
-          //console.log(x,y)
           ctx.beginPath()
           ctx.fillStyle = drawColor.value
           ctx.arc(x,y,9,0,2*Math.PI)
@@ -146,31 +124,17 @@ peer.on("open", id=>{
 }) 
 //caller
 callBtn.addEventListener('click',async ()=>{
-    console.log(remotePidF.value)
     if (remotePidF.value!=="") {
         const remotePid = remotePidF.value
         await startLocalVideo();
-        // audioStream.getTracks().forEach(track => localStream.addTrack(track))
         localVdoElmnt.srcObject.getAudioTracks().forEach(track => localStream.addTrack(track)) //<---OK
         const call = peer.call(remotePid,localStream)
         startModal.style.display = "none"
-        // document.getElementById("controls").style.display = 'block'
         cvr.style.display = "none"
         call.on("stream", stream =>{
             remoteVdoElmnt.srcObject = stream
             remoteVdoElmnt.onloadedmetadata = ()=> remoteVdoElmnt.play()
-            // console.log(stream)
         })
-        // endcallbtn.addEventListener("click",()=>{
-        //     console.log("endcall")
-        //     call.close();
-        //     location.reload();
-        // })
-        // call.on("close",()=>{
-        //     console.log("caller_videoclosed");
-        //     remoteVdoElmnt.style.display="none";
-        //     location.reload();
-        // })
     }
 })
 document.addEventListener("keypress",async(e)=>{
@@ -181,33 +145,20 @@ document.addEventListener("keypress",async(e)=>{
 })
 //receiver
 peer.on("call", async(call)=>{
-    console.log("got a call")
     await startLocalVideo()
     startModal.style.display = "none"
-    // document.getElementById("controls").style.display = 'block'
     cvr.style.display = "none"
+    localVdoElmnt.srcObject.getAudioTracks().forEach(track => localStream.addTrack(track)) 
     call.answer(localStream)
     call.on("stream", stream =>{
         remoteVdoElmnt.srcObject = stream
         remoteVdoElmnt.onloadedmetadata = ()=> remoteVdoElmnt.play()
     })
-    // endcallbtn.addEventListener("click",()=>{
-    //     console.log("call ended")
-    //     call.close();
-    //     location.reload();
-    // })
-    // call.on("close",()=>{
-    //     console.log("receiver_videoclosed");
-    //     remoteVdoElmnt.style.display="none";
-    //     location.reload();
-        
-    // })
 })
 function endCall(){
   location.reload()
 }
 peer.on('disconnected',()=>{
-    console.log('dissed')
     location.reload()
 })
 
@@ -249,9 +200,7 @@ function muteafn(){
    
   if (!mutea){
     localStream.getAudioTracks()[0].enabled = false
-    console.log("mute")
     mutea=true;
-    console.log(mutea)
     muteabtn.innerHTML=""
     muteabtn.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mic-mute" viewBox="0 0 16 16">
     <path d="M13 8c0 .564-.094 1.107-.266 1.613l-.814-.814A4.02 4.02 0 0 0 12 8V7a.5.5 0 0 1 1 0v1zm-5 4c.818 0 1.578-.245 2.212-.667l.718.719a4.973 4.973 0 0 1-2.43.923V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 1 0v1a4 4 0 0 0 4 4zm3-9v4.879l-1-1V3a2 2 0 0 0-3.997-.118l-.845-.845A3.001 3.001 0 0 1 11 3z"/>
@@ -262,7 +211,6 @@ function muteafn(){
   if(mutea){
     localStream.getAudioTracks()[0].enabled = true
     mutea=false;
-    console.log(mutea)
     muteabtn.innerHTML=""
     muteabtn.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mic" viewBox="0 0 16 16">
     <path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5z"/>
@@ -276,9 +224,7 @@ function mutevfn(){
    
     if (!mutev){
       localStream.getVideoTracks()[0].enabled = false
-      console.log("mutev")
       mutev=true;
-      console.log(mutev)
       mutevbtn.innerHTML=""
       mutevbtn.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-camera-video-off" viewBox="0 0 16 16">
       <path fill-rule="evenodd" d="M10.961 12.365a1.99 1.99 0 0 0 .522-1.103l3.11 1.382A1 1 0 0 0 16 11.731V4.269a1 1 0 0 0-1.406-.913l-3.111 1.382A2 2 0 0 0 9.5 3H4.272l.714 1H9.5a1 1 0 0 1 1 1v6a1 1 0 0 1-.144.518l.605.847zM1.428 4.18A.999.999 0 0 0 1 5v6a1 1 0 0 0 1 1h5.014l.714 1H2a2 2 0 0 1-2-2V5c0-.675.334-1.272.847-1.634l.58.814zM15 11.73l-3.5-1.555v-4.35L15 4.269v7.462zm-4.407 3.56-10-14 .814-.58 10 14-.814.58z"/>
@@ -288,7 +234,6 @@ function mutevfn(){
     if(mutev){
       localStream.getVideoTracks()[0].enabled = true
       mutev=false;
-      console.log(mutev)
       mutevbtn.innerHTML=""
       mutevbtn.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-camera-video" viewBox="0 0 16 16">
       <path fill-rule="evenodd" d="M0 5a2 2 0 0 1 2-2h7.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 4.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 13H2a2 2 0 0 1-2-2V5zm11.5 5.175 3.5 1.556V4.269l-3.5 1.556v4.35zM2 4a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h7.5a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1H2z"/>
